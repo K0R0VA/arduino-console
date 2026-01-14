5ccc6eeef829a18e1cd38ab22487f64b05292896
mic <mic>
2026-01-14 12:03:23 +0300

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ macro_rules! print {
     ($($t:tt)*) => {
         avr_device::interrupt::free(
             |cs| {
-                if let Some(console) = $crate::CONSOLE.borrow(cs).borrow_mut().as_mut() {
+                if let Some(console) = arduino_console::CONSOLE.borrow(cs).borrow_mut().as_mut() {
                     let _ = ufmt::uwrite!(console, $($t)*);
                 }
             },
@@ -44,7 +44,7 @@ macro_rules! println {
     ($($t:tt)*) => {
         avr_device::interrupt::free(
             |cs| {
-                if let Some(console) = $crate::CONSOLE.borrow(cs).borrow_mut().as_mut() {
+                if let Some(console) = arduino_console::CONSOLE.borrow(cs).borrow_mut().as_mut() {
                     let _ = ufmt::uwriteln!(console, $($t)*);
                 }
             },
